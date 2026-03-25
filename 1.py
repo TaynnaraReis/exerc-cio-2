2496adb021ffefda8c29c5fb6821c6445265766f
Taynnara Sousa <taynnara19187456@alunos.sc.senac.br>
2026-03-25 20:29:33 -0300

    lista

---
+++ 1.py
@@ -0,0 +1,9 @@
+frutas = ['manga', 'abacate', 'bacaba', 'graviola']
+numeros = [1, 2, 3, 4]
+print(frutas)
+
+frutas.append('açai')
+print('Após adicionar:', frutas)
+
+frutas.remove('abacate')
+print("Após remover 'abacate':", frutas)
