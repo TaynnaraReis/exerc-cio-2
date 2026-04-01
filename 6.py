e7c3e88ae8c1e313ea605863d1a134b01782735e
TaynnaraReis <tay52sousa@gmail.com>
2026-03-31 22:17:00 -0300

    ativ

--- 6.py
+++ 6.py
@@ -2,15 +2,8 @@
 nome = input("Digite o nome do aluno: ")
 idade = int(input("Digite a idade do aluno: "))
 
-# Criar dicionário
 aluno = {"nome": nome, "idade": idade}
 
-# Exibir dicionário
 print("Dicionário do aluno:", aluno)
 
-# Acesso por chave
-print("Nome:", aluno["nome"])
-print("Idade:", aluno["idade"])
-
-# Exibir tipo
 print("Tipo da variável aluno:", type(aluno))
